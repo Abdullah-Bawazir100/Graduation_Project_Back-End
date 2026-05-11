c429dac5b4882480e1bfc6e248e9d729423f2000
Salem Alswil <sa.al9wel@gmail.com>
2026-05-11 05:49:23 +0300

    small fix

--- app/Application/TaxInformation/UseCases/ListTaxInformationsUseCase.php
+++ app/Application/TaxInformation/UseCases/ListTaxInformationsUseCase.php
@@ -3,11 +3,15 @@
 namespace App\Application\TaxInformation\UseCases;
 
 use App\Domain\TaxInformation\Repositories\TaxInformationRepositoryInterface;
+use App\Domain\TaxPayer\Repositories\TaxPayerRepositoryInterface;
+use App\Domain\User\Repositories\UserRepositoryInterface;
 
 class ListTaxInformationsUseCase
 {
     public function __construct(
-        private TaxInformationRepositoryInterface $tax_information_repository
+        private TaxInformationRepositoryInterface $tax_information_repository,
+        private TaxPayerRepositoryInterface $tax_payer_repository,
+        private UserRepositoryInterface $user_repository
     )
     {
     }
@@ -15,8 +19,30 @@ class ListTaxInformationsUseCase
     public function execute()
     {
         $taxInformations = $this->tax_information_repository->getAll();
-        return [
-            'TaxInformations' => $taxInformations,
-        ];
+
+        $result = [];
+        foreach ($taxInformations as $taxInfo) {
+            $taxPayer = null;
+            $taxPayerUserInfo = null;
+
+            if (!empty($taxInfo->tax_payer_id)) {
+                $taxPayer = $this->tax_payer_repository->findById($taxInfo->tax_payer_id);
+
+                if ($taxPayer && !empty($taxPayer->userId)) {
+                    $user = $this->user_repository->findById($taxPayer->userId);
+                    if ($user) {
+                        $taxPayerUserInfo = $user;
+                    }
+                }
+            }
+
+            $result[] = [
+                'taxInfo' => $taxInfo,
+                'taxPayerInfo' => $taxPayer,
+                'userInfo' => $taxPayerUserInfo ? $taxPayerUserInfo->toArray() : null,
+            ];
+        }
+
+        return $result;
     }
 }
