704ebd0a5c6a127c20b8c18bfbc9a9979e83418c
AbdullahBawazir <bawzyr339@gmail.com>
2026-06-04 18:54:00 +0300

    Add Spatie/laravel-pdf pachage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\LaravelPdf\Facades\Pdf;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/pdf-test', function () {
+    return Pdf::html('
+        <h1>Hello PDF</h1>
+        <p>Laravel PDF is working!</p>
+    ');
 });
