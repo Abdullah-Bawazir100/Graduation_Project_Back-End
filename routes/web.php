4600f78fdf840e604dbb72ca8a963aeda314182c
AbdullahBawazir <bawzyr339@gmail.com>
2026-06-24 05:34:52 +0300

    Refactor web.php file

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,16 @@ use Spatie\LaravelPdf\Facades\Pdf;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-link', function () {
+    $target = storage_path('app/public');
+    $shortcut = public_path('storage');
+
+    if(file_exists($shortcut))
+    {
+        return 'The "public/storage" directory already exists.';
+    }
+
+    symlink($target, $shortcut);
+    return 'The "public/storage" directory has been created successfully.';
+});
