70823cc99819cad626b84b4ecbc698ba1ec7b500
AbdullahBawazir <bawzyr339@gmail.com>
2026-06-29 00:25:34 +0300

    Some Updates

--- config/cors.php
+++ config/cors.php
@@ -3,16 +3,16 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
+        |--------------------------------------------------------------------------
+        | Cross-Origin Resource Sharing (CORS) Configuration
+        |--------------------------------------------------------------------------
+        |
+        | Here you may configure your settings for cross-origin resource sharing
+        | or "CORS". This determines what cross-origin operations may execute
+        | in web browsers. You are free to adjust these settings as needed.
+        |
+        | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+        |
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
@@ -21,6 +21,7 @@ return [
 
     'allowed_origins' => [
         'https://hadhramaut-tax-office.vercel.app',
+        'https://www.hadhramaut-tax-office.vercel.app',
         'http://localhost:5173',
     ],
 
@@ -30,7 +31,7 @@ return [
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => true,
 
