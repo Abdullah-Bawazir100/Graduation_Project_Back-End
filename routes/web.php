392affd6c6b3b293f7c27e2ad3fd3acfbaa0d621
AbdullahBawazir <bawzyr339@gmail.com>
2026-07-04 03:35:24 +0300

    Some Updates

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPdf\Facades\Pdf;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Response;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,4 +30,21 @@ Route::get('/run-link', function () {
 
     return 'Failed to create storage link. please check folder permissions.';
 
+});
+
+
+Route::get('storage/{folder}/{filename}', function ($folder, $filename) {
+    $path = storage_path('app/public/' . $folder . '/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    $file = file_get_contents($path);
+    $type = mime_content_type($path);
+
+    return Response::make($file, 200, [
+        'Content-Type' => $type,
+        'Content-Disposition' => 'inline; filename="' . $filename . '"'
+    ]);
 });
